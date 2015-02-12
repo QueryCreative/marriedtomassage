20e9530ab60ad2b05a2670fc13095255fc7c7c75
Joshua Schmitz <js@the42.com>
2015-02-12 17:54:56 -0600

    more view migration, not finished - also condense asset image directory, remove folders, all images in one directory

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,12 +1,12 @@
 $(function() {
 	window.preloader = new Preloadit({
 		images: [
-			"/assets/temp/video-placeholder2.png",
-			"/assets/building-home.jpg",
-			"/assets/pano1.jpg",
-			"/assets/history-wall.jpg",
-			"/assets/datacenter.jpg",
-			"/assets/temp/map-img.png"
+			"video-placeholder2.png",
+			"building-home.jpg",
+			"pano1.jpg",
+			"history-wall.jpg",
+			"datacenter.jpg",
+			"map-img.png"
 		], 
 		onComplete: function(){
 			$("#loader").delay(3200).fadeOut(1000);
@@ -15,5 +15,5 @@ $(function() {
 });
 
 $(document).ready(function () {
-	$("#loader-gif").load().attr("src", "/assets/Netrality_2_1_second_delay.gif");
+	$("#loader-gif").load().attr("src", "Netrality_2_1_second_delay.gif");
 });
