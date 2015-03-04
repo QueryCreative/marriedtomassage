8102810f951260c9f0fa31879d7dc24792a8502d
Brandon Fulk <bf@the42.com>
2015-03-04 11:37:32 -0600

    Updated to include slick init again

---
+++ app/assets/javascripts/slick_init.js
@@ -0,0 +1,81 @@
+$(document).on('ready page:load', function(){
+
+	$('#about-slider').slick({
+		infinite: true,
+		arrows: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		slide: '.slide',
+		responsive: [
+			{
+				breakpoint: 2560,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+		    {
+		    	breakpoint: 880,
+		    	settings: {
+		    		slidesToShow: 2,
+		    		slidesToScroll: 1
+		    	}
+			},
+			{
+		    	breakpoint: 560,
+		    	settings: {
+		    		slidesToShow: 1,
+		    		slidesToScroll: 1
+		   		}
+			}
+		]
+	});
+
+	$('#city-slider').slick({
+		infinite: true,
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		slide: '.slide',
+		responsive: [
+			{
+				breakpoint: 2560,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+		    {
+		    	breakpoint: 880,
+		    	settings: {
+		    		slidesToShow: 2,
+		    		slidesToScroll: 1
+		    	}
+			},
+			{
+		    	breakpoint: 560,
+		    	settings: {
+		    		slidesToShow: 1,
+		    		slidesToScroll: 1
+		   		}
+			}
+		]
+	});
+
+	$('#main-slider').slick({
+		infinite: true,
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		slide: '.slide'
+	});
+
+});
