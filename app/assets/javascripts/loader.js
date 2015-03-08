d22cdd92342cbb0f3ed43e7392b80157c3287680
Joshua Schmitz <js@the42.com>
2015-03-07 22:41:36 -0600

    adds google and imbedded fonts ("Roboto Slab" and "Webly Sleeky UI"), font files/import css included, removes extraneous files (js/coffee and images), modifies css - deletes most unnecessary styles, add image assets, updates google map styles (also map-icon and default map image), removes unneeded views, updates nav>ul, updates google analytics and meta tags (SEO), removes slick slider

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,12 +1,7 @@
 $(function() {
 	window.preloader = new Preloadit({
 		images: [
-			"/assets/video-placeholder2.png",
-			"/assets/building-home.jpg",
-			"/assets/pano1.jpg",
-			"/assets/history-wall.jpg",
-			"/assets/datacenter.jpg",
-			"/assets/map-img.png"
+			"/assets/logo.png",
 		], 
 		onComplete: function(){
 			$("#loader").delay(3200).fadeOut(1000);
@@ -15,5 +10,5 @@ $(function() {
 });
 
 $(document).ready(function () {
-	$("#loader-gif").load().attr("src", "Netrality_2_1_second_delay.gif");
+	$("#loader-gif").load().attr("src", "loader-gif.gif");
 });
