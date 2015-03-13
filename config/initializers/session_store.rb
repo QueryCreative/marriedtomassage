ef7d162567a1525eb3ef2c779209dd656576867c
Joshua Schmitz <js@the42.com>
2015-03-13 14:53:12 -0500

    re-re-re-refactor

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_netonrails_session'
+Rails.application.config.session_store :cookie_store, key: '_m2m_session'
