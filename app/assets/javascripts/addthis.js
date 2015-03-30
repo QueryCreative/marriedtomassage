91a38250a69058b710968944769d63d1768df525
Joshua Schmitz <js@the42.com>
2015-03-30 17:03:04 -0500

    fuck it - going back from add this to charters - god dammit

--- app/assets/javascripts/addthis.js
+++ app/assets/javascripts/addthis.js
@@ -1,35 +1,35 @@
-//fixes social addthis links
-var addthis;
-addthis = function() {
-    var script = 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55181f164231a203#async=1';
-      // Remove all global properties set by addthis, otherwise it won't reinitialize
-      for (var i in window) {
-        if (/^addthis/.test(i) || /^_at/.test(i)) {
-          delete window[i];
-        }
-      }
-      window.addthis_share = null;
+// //fixes social addthis links
+// var addthis;
+// addthis = function() {
+//     var script = 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55181f164231a203#async=1';
+//       // Remove all global properties set by addthis, otherwise it won't reinitialize
+//       for (var i in window) {
+//         if (/^addthis/.test(i) || /^_at/.test(i)) {
+//           delete window[i];
+//         }
+//       }
+//       window.addthis_share = null;
 
-      // Remove all global properties set by addthis, otherwise it won't reinitialize
-      if(window.addthis) {
-           window.addthis = null;
-            window._adr = null;
-            window._atc = null;
-            window._atd = null;
-            window._ate = null;
-            window._atr = null;
-            window._atw = null;
-      }
+//       // Remove all global properties set by addthis, otherwise it won't reinitialize
+//       if(window.addthis) {
+//            window.addthis = null;
+//             window._adr = null;
+//             window._atc = null;
+//             window._atd = null;
+//             window._ate = null;
+//             window._atr = null;
+//             window._atw = null;
+//       }
 
-      // Finally, load addthis
-      $.getScript(script, function(){
-        addthis.toolbox('.addthis_toolbox');
-      });
-}
+//       // Finally, load addthis
+//       $.getScript(script, function(){
+//         addthis.toolbox('.addthis_toolbox');
+//       });
+// }
 
-$(document).ready(addthis);
-$(document).on('page:load', addthis);
-$(document).on('page:change', addthis);
+// $(document).ready(addthis);
+// $(document).on('page:load', addthis);
+// $(document).on('page:change', addthis);
 
 
 // // turbolinks addthis
@@ -45,7 +45,7 @@ $(document).on('page:change', addthis);
 //     window.addthis_share = null;
  
 //     // Finally, load addthis
-//     $.getScript("http://s7.addthis.com/js/300/addthis_widget.js#pubid=YOUR-PUBLISHER-ID");
+//     $.getScript("https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55181f164231a203");
 // }
  
 // // Trigger the function on both jquery's ready event and turbolinks page:change event
