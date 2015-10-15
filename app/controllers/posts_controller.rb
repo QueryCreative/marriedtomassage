e53248e078d93a6a1304b569c8bf7faaded76cc3
Joshua Schmitz <js@the42.com>
2015-10-15 10:47:32 -0500

    removed table/model_name

--- app/controllers/posts_controller.rb
+++
@@ -1,9 +0,0 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.order("created_at DESC")
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001183255) do
+ActiveRecord::Schema.define(version: 20151015154151) do
 
   create_table "announcements", force: :cascade do |t|
     t.text     "content"
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20151001183255) do
     t.string   "title"
     t.string   "external_link"
     t.datetime "published_at"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "specials", force: :cascade do |t|
